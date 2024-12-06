5b4d81ef056a429f8a09ef36c9115ca011fd2e33
nhím <94636211+Meriadoc-gitgit@users.noreply.github.com>
2024-12-06 05:09:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,30 @@
+function skillsMember() {
+    let skills = [
+        {
+            name: 'HTML',
+            logo: 'https://cdn.worldvectorlogo.com/logos/html5.svg'
+        },
+        {
+            name: 'CSS',
+            logo: 'https://cdn.worldvectorlogo.com/logos/css-5.svg'
+        },
+        {
+            name: 'JavaScript',
+            logo: 'https://cdn.worldvectorlogo.com/logos/javascript-1.svg'
+        },
+        {
+            name: 'React',
+            logo: 'https://cdn.worldvectorlogo.com/logos/react-2.svg'
+        },
+        {
+            name: 'Node',
+            logo: 'https://cdn.worldvectorlogo.com/logos/nodejs.svg'
+        },
+        {
+            name: 'MongoDB',
+            logo: 'https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg'
+        }
+    ];
+
+    return skills;
+}
